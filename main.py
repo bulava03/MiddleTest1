30eb996830e505b0dc0eefb5f2a63a6e8e940be2
Hennadii Bulava <bulava03@knu.ua>
2023-03-30 09:21:55 +0300

    Added counting words

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import re
 
+
 def read_file(file_path):
     with open(file_path, 'r') as file:
         data = file.read().replace('\n', ' ')
     return data
 
 
+def count_words(text):
+    words = re.findall(r'\b\w+\b', text.lower())
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+
+if __name__ == '__main__':
+    file_path = 'example.txt'
+    output_file_path = 'output.txt'
+    text = read_file(file_path)
