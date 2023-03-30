ccdf180ada89ad2219acab7f04ae737adc478256
Hennadii Bulava <bulava03@knu.ua>
2023-03-30 09:28:16 +0300

    Added output

--- main.py
+++ main.py
@@ -23,7 +23,20 @@ def sort_words(word_count):
     return sorted_words[:10]
 
 
+def write_file(word_count, output_file_path):
+    counter = 0
+    with open(output_file_path, 'w') as file:
+        for word, count in word_count:
+            file.write(f'{word}: {count}\n')
+            counter += 1
+            if counter == 10:
+                break
+
+
 if __name__ == '__main__':
     file_path = 'example.txt'
     output_file_path = 'output.txt'
     text = read_file(file_path)
+    words = count_words(text)
+    sorted_words = sort_words(words)
+    write_file(sorted_words, output_file_path)
